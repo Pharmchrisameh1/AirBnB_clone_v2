0c168634e589a4b54b614af2e36a35926e7803e4
Rx Chris Ameh <chris1jnr@gmail.com>
2025-01-06 13:58:21 -0800

    updated files

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,32 +1,24 @@
 #!/usr/bin/python3
-"""Defines the unittests for the City model."""
-
-import os
-from models.city import City
+""" """
 from tests.test_models.test_base_model import test_basemodel
+from models.city import City
 
 
-class test_city(test_basemodel):
-    """A unittest for City class."""
+class test_City(test_basemodel):
+    """ """
 
     def __init__(self, *args, **kwargs):
-        """Initialize the test class for City."""
+        """ """
         super().__init__(*args, **kwargs)
         self.name = "City"
         self.value = City
 
     def test_state_id(self):
-        """Tests the type of state_id attribute."""
-        new = self.value()
-        self.assertEqual(
-            type(new.state_id),
-            str if os.getenv("HBNB_TYPE_STORAGE") != "db" else type(None),
-        )
+        """ """
+        new = self.value(**{'state_id': '50'})
+        self.assertEqual(type(new.state_id), str)
 
     def test_name(self):
-        """Tests the type of name attribute."""
-        new = self.value()
-        self.assertEqual(
-            type(new.name),
-            str if os.getenv("HBNB_TYPE_STORAGE") != "db" else type(None),
-        )
+        """ """
+        new = self.value(**{'name': 'Alexandria'})
+        self.assertEqual(type(new.name), str)
