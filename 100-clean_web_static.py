b139ca5aefbfc31e1e347baee955437a40d2216f
Rx Chris Ameh <chris1jnr@gmail.com>
2025-01-27 19:29:34 -0800

    updated files

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,36 +1,29 @@
 #!/usr/bin/python3
-"""This module defines the function do_clean
-"""
-from fabric.api import *
+"""Clean up old archives and releases in the web_static deployment."""
+import os
+from fabric.api import env, run, sudo, lcd, local, cd
 
-
-env.hosts = ['34.207.212.22', '54.237.31.63']
-env.user = 'ubuntu'
+env.hosts = ["34.207.212.22", "54.237.31.63"]
+env.user = "ubuntu"
+env.key_filename = "~/.ssh/id_rsa_alx"
 
 
 def do_clean(number=0):
-    """This function deletes out-of-date archives
     """
-    output = local("ls -lt ./versions/", capture=True)
-    output = output.split('\n')
-    output.pop(0)
-    number = int(number)
-    if number == 0:
-        number = 1
-    i = number
-    while i <= (len(output) - 1):
-        filename = output[i].split(' ')[-1]
-        local("rm -rf ./versions/{}".format(filename))
-        i += 1
+    Clean up old archives and releases in the web_static deployment.
 
-    output = sudo("ls -lt /data/web_static/releases")
-    output = output.split('\n')
-    output.pop(0)
-    i = number
-    delstr = ""
-    while i <= (len(output) - 1):
-        fil = output[i].split(' ')[-1].replace('\r', '')
-        filename = " /data/web_static/releases/" + fil + " "
-        delstr += filename
-        i += 1
-    sudo("rm -rf {}".format(delstr))
+    Args:
+        number (int): The number of archives and releases to keep.
+            If not provided or 0, it keeps 1 archive and release.
+    """
+    nbr = 1 if int(number) == 0 else int(number)
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(nbr)]
+    with lcd("versions"):
+        [local("rm ./{}".format(file)) for file in archives]
+
+    with cd("/data/web_static/releases"):
+        archives = run("ls -tr").split()
+        archives = [file for file in archives if "web_static_" in file]
+        [archives.pop() for i in range(nbr)]
+        [sudo("rm -rf ./{}".format(file)) for file in archives]
